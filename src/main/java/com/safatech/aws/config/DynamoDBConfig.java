3f82a23bb6d367fcff2262cdacb8c612ab5ba838
Saddam-Pc <saddam8101.sk@gmail.com>
2024-05-25 13:31:25 +0530

    first commit

---
+++ src/main/java/com/safatech/aws/config/DynamoDBConfig.java
@@ -0,0 +1,33 @@
+package com.safatech.aws.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+// ctrl+Alt+c
+@Configuration
+public class DynamoDBConfig {
+
+    public static final String SERVICE_ENDPOINT = "dynamodb.ap-south-1.amazonaws.com";
+    public static final String REGION = "ap-south-1";
+    public static final String ACCESS_KEY = "AKIA5FTZA7MD4LNNKKSK";
+    public static final String SECRET_KEY = "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";
+
+    @Bean
+    public DynamoDBMapper mapper() {
+        return new DynamoDBMapper(amazonDynamoDBConfig());
+    }
+
+    private AmazonDynamoDB amazonDynamoDBConfig() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(SERVICE_ENDPOINT, REGION))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)))
+                .build();
+    }
+}
